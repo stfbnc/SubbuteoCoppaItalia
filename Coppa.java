7de1b4adf998be409926046881b6696e73bbb8f2
stfbnc <41962445+stfbnc@users.noreply.github.com>
2019-02-19 14:04:54 +0100

    modifiche 19/2/2019

--- Coppa.java
+++ Coppa.java
@@ -42,44 +42,57 @@ public class Coppa{
         JFrame main_window = new JFrame("COPPA ITALIA SUBBUTEO");
         main_window.setContentPane(new JLabel(new GUIcompon().ResizeImg(imPath, "campo2.png", 1150, 800)));
         Connection conn = new SQLiteOps().SQLconn(db);
+	    
+	//CERCARE DI PRENDERE TUTTO QUESTO IN UN LOOP
+	//CLASSIFICA DINAMICA CHE SI ALLARGA IN BASE AI GIOCATORI CHE TROVA (TIPO STRUCT)
+	    
         //########################################################################SPAREGGIO PRELIMINARE
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("SP_PREL"), 1);
         results = new SQLiteOps().GetResults(conn, dbTypes.get("SP_PREL"), 1);
-        JTextField[] resSpareggioPrel = new GUIcompon().MatchResults(main_window, 95, 65, results[0], results[1], results[2], results[3], 1);
-        JTextPane[] txtSpareggioPrel = new GUIcompon().MatchPlayers(main_window, 95, 65, players[0], players[1], "SPAREGGIO PRELIMINARE");
+        JTextField[] resSpareggioPrel = new GUIcompon().MatchResults(main_window, 95, 65, results, 1);
+        JTextPane[] txtSpareggioPrel = new GUIcompon().MatchPlayers(main_window, 95, 65, players, "SPAREGGIO PRELIMINARE");
         //########################################################################SPAREGGI
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("SPAR"), 1);
-        JTextField[] resSpareggio1 = new GUIcompon().MatchResults(main_window, 95, 125, results[0], results[1], results[2], results[3], 1);
-        JTextPane[] txtSpareggio1 = new GUIcompon().MatchPlayers(main_window, 95, 125, players[0], players[1], "SPAREGGI");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("SPAR"), 1);
+        JTextField[] resSpareggio1 = new GUIcompon().MatchResults(main_window, 95, 125, results, 1);
+        JTextPane[] txtSpareggio1 = new GUIcompon().MatchPlayers(main_window, 95, 125, players, "SPAREGGI");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("SPAR"), 2);
-        JTextField[] resSpareggio2 = new GUIcompon().MatchResults(main_window, 95, 125+yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtSpareggio2 = new GUIcompon().MatchPlayers(main_window, 95, 125+yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("SPAR"), 2);
+        JTextField[] resSpareggio2 = new GUIcompon().MatchResults(main_window, 95, 125+yHeight, results, 0);
+        JTextPane[] txtSpareggio2 = new GUIcompon().MatchPlayers(main_window, 95, 125+yHeight, players, "");
         //########################################################################GIRONE
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 1);
-        JTextField[] resGirone1 = new GUIcompon().MatchResults(main_window, startX, startY, results[0], results[1], results[2], results[3], 1);
-        JTextPane[] txtGirone1 = new GUIcompon().MatchPlayers(main_window, startX, startY, players[0], players[1], "GIRONE PRINCIPALE");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 1);
+        JTextField[] resGirone1 = new GUIcompon().MatchResults(main_window, startX, startY, results, 1);
+        JTextPane[] txtGirone1 = new GUIcompon().MatchPlayers(main_window, startX, startY, players, "GIRONE PRINCIPALE");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 2);
-        JTextField[] resGirone2 = new GUIcompon().MatchResults(main_window, startX, startY+yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtGirone2 = new GUIcompon().MatchPlayers(main_window, startX, startY+yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 2);
+        JTextField[] resGirone2 = new GUIcompon().MatchResults(main_window, startX, startY+yHeight, results, 0);
+        JTextPane[] txtGirone2 = new GUIcompon().MatchPlayers(main_window, startX, startY+yHeight, players, "");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 3);
-        JTextField[] resGirone3 = new GUIcompon().MatchResults(main_window, startX, startY+2*yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtGirone3 = new GUIcompon().MatchPlayers(main_window, startX, startY+2*yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 3);
+        JTextField[] resGirone3 = new GUIcompon().MatchResults(main_window, startX, startY+2*yHeight, results, 0);
+        JTextPane[] txtGirone3 = new GUIcompon().MatchPlayers(main_window, startX, startY+2*yHeight, players, "");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 4);
-        JTextField[] resGirone4 = new GUIcompon().MatchResults(main_window, startX, startY+3*yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtGirone4 = new GUIcompon().MatchPlayers(main_window, startX, startY+3*yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 4);
+        JTextField[] resGirone4 = new GUIcompon().MatchResults(main_window, startX, startY+3*yHeight, results, 0);
+        JTextPane[] txtGirone4 = new GUIcompon().MatchPlayers(main_window, startX, startY+3*yHeight, players, "");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 5);
-        JTextField[] resGirone5 = new GUIcompon().MatchResults(main_window, startX, startY+4*yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtGirone5 = new GUIcompon().MatchPlayers(main_window, startX, startY+4*yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 5);
+        JTextField[] resGirone5 = new GUIcompon().MatchResults(main_window, startX, startY+4*yHeight, results, 0);
+        JTextPane[] txtGirone5 = new GUIcompon().MatchPlayers(main_window, startX, startY+4*yHeight, players, "");
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("GIR"), 6);
-        JTextField[] resGirone6 = new GUIcompon().MatchResults(main_window, startX, startY+5*yHeight, results[0], results[1], results[2], results[3], 0);
-        JTextPane[] txtGirone6 = new GUIcompon().MatchPlayers(main_window, startX, startY+5*yHeight, players[0], players[1], "");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("GIR"), 6);
+        JTextField[] resGirone6 = new GUIcompon().MatchResults(main_window, startX, startY+5*yHeight, results, 0);
+        JTextPane[] txtGirone6 = new GUIcompon().MatchPlayers(main_window, startX, startY+5*yHeight, players, "");
         JTextPane Table = new GUIcompon().TableContainer();
         Table.setBounds(startX, startY+6*yHeight, 4*30+2*140, 4*yHeight);
         main_window.add(Table);
         //########################################################################FINALE
         players = new SQLiteOps().GetPlayers(conn, dbTypes.get("FIN"), 1);
-        JTextField[] resFinale = new GUIcompon().MatchResults(main_window, 655, 640, results[0], results[1], results[2], results[3], 1);
-        JTextPane[] txtFinale = new GUIcompon().MatchPlayers(main_window, 655, 640, players[0], players[1], "FINALE");
+	results = new SQLiteOps().GetResults(conn, dbTypes.get("FIN"), 1);
+        JTextField[] resFinale = new GUIcompon().MatchResults(main_window, 655, 640, results, 1);
+        JTextPane[] txtFinale = new GUIcompon().MatchPlayers(main_window, 655, 640, players, "FINALE");
         //close connection
         new SQLiteOps().CloseCnt(conn);
         //########################################################################BOTTONE AGGIORNA
@@ -94,33 +107,53 @@ public class Coppa{
             	//if(!check){
             	//	JOptionPane.showMessageDialog(null,"Alcuni risultati non sono stati inseriti correttamente!","",JOptionPane.ERROR_MESSAGE);
             	//}else{
-					try{
+			try{
             			Connection conn = new SQLiteOps().SQLconn(db);
             			new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtSpareggioPrel[0].getText(), "td"),
-            										new ResultsOps().NoHtmlText(txtSpareggioPrel[1].getText(), "td"),
-            										resSpareggioPrel[0].getText(), resSpareggioPrel[1].getText(), resSpareggioPrel[2].getText(),
-            										resSpareggioPrel[3].getText(), dbTypes.get("SP_PREL"), 1);
-	            		/*new SQLiteOps().SQLgroup(conn, resA, A, "A");
-	            		new SQLiteOps().SQLgroup(conn, resB, B, "B");
-	            		String[] semiA = new ResultsOps().LeagueTable(tableA, new SQLiteOps().ExtractPlayersVal(conn, A));
-	            		if(semiA[2] == "0"){
-	            			new SQLiteOps().UpdateSemi(conn, "SEMI1", "SEMI2", txtSemi11, txtSemi22,
-	            									   res11, res12, res21, res22);
-	            			new SQLiteOps().UpdateFin(conn, txtFin1, txtFin2, res1, res2);
-	            		}else
-	            			new SQLiteOps().SQLsemiPlayers(conn, semiA, txtSemi11, txtSemi22, res11, res22, "SEMI1", "SEMI2");
-	            		String[] semiB = new ResultsOps().LeagueTable(tableB, new SQLiteOps().ExtractPlayersVal(conn, B));
-	            		if(semiB[2] == "0"){
-	            			new SQLiteOps().UpdateSemi(conn, "SEMI2", "SEMI1", txtSemi21, txtSemi12,
-									                   res11, res12, res21, res22);
-	            			new SQLiteOps().UpdateFin(conn, txtFin1, txtFin2, res1, res2);
-	            		}else
-	            			new SQLiteOps().SQLsemiPlayers(conn, semiB, txtSemi21, txtSemi12, res21, res12, "SEMI2", "SEMI1");
-	            		new SQLiteOps().SQLfinPlayers(conn, txtFin1, txtFin2, res1, res2);*/
+            						new ResultsOps().NoHtmlText(txtSpareggioPrel[1].getText(), "td"),
+            						resSpareggioPrel[0].getText(), resSpareggioPrel[1].getText(), resSpareggioPrel[2].getText(),
+            						resSpareggioPrel[3].getText(), dbTypes.get("SP_PREL"), 1);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtSpareggio1[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtSpareggio1[1].getText(), "td"),
+            						resSpareggio1[0].getText(), resSpareggio1[1].getText(), resSpareggio1[2].getText(),
+            						resSpareggio1[3].getText(), dbTypes.get("SPAR"), 1);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtSpareggio2[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtSpareggio2[1].getText(), "td"),
+            						resSpareggio2[0].getText(), resSpareggio2[1].getText(), resSpareggio2[2].getText(),
+            						resSpareggio2[3].getText(), dbTypes.get("SPAR"), 2);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone1[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone1[1].getText(), "td"),
+            						resGirone1[0].getText(), resGirone1[1].getText(), resGirone1[2].getText(),
+            						resGirone1[3].getText(), dbTypes.get("GIR"), 1);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone2[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone2[1].getText(), "td"),
+            						resGirone2[0].getText(), resGirone2[1].getText(), resGirone2[2].getText(),
+            						resGirone2[3].getText(), dbTypes.get("GIR"), 2);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone3[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone3[1].getText(), "td"),
+            						resGirone3[0].getText(), resGirone3[1].getText(), resGirone3[2].getText(),
+            						resGirone3[3].getText(), dbTypes.get("GIR"), 3);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone4[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone4[1].getText(), "td"),
+            						resGirone4[0].getText(), resGirone4[1].getText(), resGirone4[2].getText(),
+            						resGirone4[3].getText(), dbTypes.get("GIR"), 4);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone5[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone5[1].getText(), "td"),
+            						resGirone5[0].getText(), resGirone5[1].getText(), resGirone5[2].getText(),
+            						resGirone5[3].getText(), dbTypes.get("GIR"), 5);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtGirone6[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtGirone6[1].getText(), "td"),
+            						resGirone6[0].getText(), resGirone6[1].getText(), resGirone6[2].getText(),
+            						resGirone6[3].getText(), dbTypes.get("GIR"), 6);
+				new SQLiteOps().UpdateDbRow(conn, new ResultsOps().NoHtmlText(txtFinale[0].getText(), "td"),
+            						new ResultsOps().NoHtmlText(txtFinale[1].getText(), "td"),
+            						resFinale[0].getText(), resFinale[1].getText(), resFinale[2].getText(),
+            						resFinale[3].getText(), dbTypes.get("FIN"), 1);
+				//la classifica legge dal database
 	            		new SQLiteOps().CloseCnt(conn);
-					}catch (Exception e1){
-						e1.printStackTrace();
-					}
+			}catch (Exception e1){
+				e1.printStackTrace();
+			}
             	//}
             }
         });
@@ -130,17 +163,6 @@ public class Coppa{
         main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_window.setSize(1150,800);
         main_window.setVisible(true);
-        //########################################################################CARICAMENTO DATI ALL'APERTURA
-		/*try{
-			Connection firstConn = new SQLiteOps().SQLconn(db);
-	        new SQLiteOps().LoadData(firstConn, resA, A, resB, B, txtSemi11, txtSemi12, txtSemi21, txtSemi22,
-	        						 res11, res12, res21, res22, txtFin1, txtFin2, res1, res2);
-	        String[] uselessA = new ResultsOps().LeagueTable(tableA, new SQLiteOps().ExtractPlayersVal(firstConn, A));
-	        String[] uselessB = new ResultsOps().LeagueTable(tableB, new SQLiteOps().ExtractPlayersVal(firstConn, B));
-	    	new SQLiteOps().CloseCnt(firstConn);
-		}catch (Exception e1){
-			e1.printStackTrace();
-		}*/
     }
     
     public static void main(String[] args) throws SQLException{
